4b7274da4f9aede26293f21969788add33eae50b
DigitalCrafting <k.kasprzyk2@gmail.com>
2025-01-20 17:07:22 +0100

    [CMake] Doxygen

---
+++ 2-learning-c-make/9-doxygen/src/my_lib/my_lib.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include <cxxopts.hpp>
+#include <fmt/format.h>
+#include <spdlog/spdlog.h>
+#include <nlohmann/json.hpp>
+
+#include "my_lib.h"
+
+void print_hello_world() {
+    std::cout << "Hello world!\n";
+
+    std::cout << "FMT:" << FMT_VERSION << "\n";
+
+    std::cout << "CXXOPTS:" << CXXOPTS__VERSION_MAJOR << "." << CXXOPTS__VERSION_MINOR << "." << CXXOPTS__VERSION_PATCH
+              << "\n";
+
+    std::cout << "SPDLOG:" << SPDLOG_VER_MAJOR << "." << SPDLOG_VER_MINOR << "." << SPDLOG_VER_PATCH << "\n";
+
+
+    std::cout << "JSON Lib Version: "
+              << NLOHMANN_JSON_VERSION_MAJOR
+              << "."
+              << NLOHMANN_JSON_VERSION_MINOR
+              << "."
+              << NLOHMANN_JSON_VERSION_PATCH
+              << '\n';
+}
+
