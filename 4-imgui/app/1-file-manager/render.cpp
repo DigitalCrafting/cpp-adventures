4363ff309164f79f5b3546092718a5686cd0e50d
DigitalCrafting <k.kasprzyk2@gmail.com>
2025-01-28 20:46:05 +0100

    [ImGui] FileManager - WIP - part 3

--- 4-imgui/app/1-file-manager/render.cpp
+++ 4-imgui/app/1-file-manager/render.cpp
@@ -64,7 +64,33 @@ void WindowClass::DrawContent() {
 }
 
 void WindowClass::DrawActions() {
-    ImGui::Text("DrawActions");
+    if (fs::is_directory(selectedEntry)) {
+        ImGui::Text("Selected dir: %s", selectedEntry.string().c_str());
+    } else if (fs::is_regular_file(selectedEntry)) {
+        ImGui::Text("Selected file: %s", selectedEntry.string().c_str());
+    } else {
+        ImGui::Text("Nothing selected");
+    }
+
+    if (fs::is_regular_file(selectedEntry) && ImGui::Button("Open")) {
+        openFileWithDefaultEditor();
+    }
+
+
+    ImGui::SameLine();
+    if (ImGui::Button("Rename")) {
+        renameDialogOpen = true;
+        ImGui::OpenPopup("Rename File");
+    }
+
+    ImGui::SameLine();
+    if (ImGui::Button("Delete")) {
+        deleteDialogOpen = true;
+        ImGui::OpenPopup("Delete File");
+    }
+
+    renameFilePopup();
+    deleteFilePopup();
 }
 
 void WindowClass::DrawFilter() {
@@ -78,7 +104,7 @@ void WindowClass::DrawFilter() {
         return;
     }
 
-    auto filtered_file_count = (unsigned int){0};
+    auto filtered_file_count = (unsigned int) 0;
     for (const auto &entry : fs::directory_iterator(currentPath)) {
         if (!fs::is_regular_file(entry)) {
             continue;
@@ -92,8 +118,84 @@ void WindowClass::DrawFilter() {
     ImGui::Text("Number of files: %u", filtered_file_count);
 }
 
+void WindowClass::openFileWithDefaultEditor() {
+#ifdef _WIN32
+    const auto command = "start \"\" \"" + selectedEntry.string() + "\"";
+#elif __APPLE__
+    const auto command = "open \"" + selectedEntry.string() + "\"";
+#else
+    const auto command = "xdg-open \"" + selectedEntry.string() + "\"";
+#endif
+    std::system(command.c_str());
+}
+
+void WindowClass::renameFilePopup() {
+    if (ImGui::BeginPopupModal("Rename File", &renameDialogOpen)) {
+        static char buffer_name[512] = {'\0'};
+        ImGui::Text("New name: ");
+        ImGui::InputText("###newName", buffer_name, sizeof(buffer_name));
+
+        if (ImGui::Button("Rename")) {
+            auto new_path = selectedEntry.parent_path() / buffer_name;
+            if (renameFile(selectedEntry, new_path)) {
+                renameDialogOpen = false;
+                selectedEntry = new_path;
+                std::memset(buffer_name, 0, sizeof(buffer_name));
+            }
+        }
+
+        ImGui::SameLine();
+
+        if (ImGui::Button("Cancel")) {
+            renameDialogOpen = false;
+        }
+
+        ImGui::EndPopup();
+    }
+}
+
+void WindowClass::deleteFilePopup() {
+    if (ImGui::BeginPopupModal("Delete File", &deleteDialogOpen)) {
+        ImGui::Text("Are you sure you want to delete %s?", selectedEntry.filename().string().c_str());
+
+        if (ImGui::Button("Yes")) {
+            if (deleteFile(selectedEntry)) {
+                selectedEntry.clear();
+            }
+            deleteDialogOpen = false;
+        }
+
+        ImGui::SameLine();
+
+        if (ImGui::Button("No")) {
+            deleteDialogOpen = false;
+        }
+
+        ImGui::EndPopup();
+    }
+}
+
+bool WindowClass::renameFile(const fs::path &old_path, const fs::path &new_path) {
+    try {
+        fs::rename(old_path, new_path);
+        return true;
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << '\n';
+        return false;
+    }
+}
+
+bool WindowClass::deleteFile(const fs::path &path) {
+    try {
+        fs::remove(path);
+        return true;
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << '\n';
+        return false;
+    }
+}
 
 void render(WindowClass &window_obj)
 {
-    window_obj.Draw("Label");
+    window_obj.Draw("File Manager");
 }
