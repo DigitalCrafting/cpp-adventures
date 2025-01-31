533a2c24e1c3044803f5a5b6d03a46eddaea709c
DigitalCrafting <k.kasprzyk2@gmail.com>
2025-01-31 12:49:27 +0100

    [ImGui] TextEditor - part 1

--- 4-imgui/app/3-text-editor/render.cpp
+++ 4-imgui/app/3-text-editor/render.cpp
@@ -1,27 +1,47 @@
 #include <imgui.h>
 #include <valarray>
 #include <implot.h>
+#include <filesystem>
 
 #include "render.hpp"
 #include "fmt/format.h"
 
-void WindowClass::Draw(std::string_view label)
-{
-    constexpr static auto window_flags =
-            ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
-            ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoScrollbar;
-    constexpr static auto window_size = ImVec2(1280.0F, 720.0F);
-    constexpr static auto window_pos = ImVec2(0.0F, 0.0F);
+namespace fs = std::filesystem;
 
-    ImGui::SetNextWindowSize(window_size);
-    ImGui::SetNextWindowPos(window_pos);
+namespace ImGuiTextEditor {
+    void WindowClass::Draw(std::string_view label) {
+        constexpr static auto window_flags =
+                ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
+                ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoScrollbar;
+        constexpr static auto window_size = ImVec2(1280.0F, 720.0F);
+        constexpr static auto window_pos = ImVec2(0.0F, 0.0F);
 
-    ImGui::Begin(label.data(), nullptr, window_flags);
+        ImGui::SetNextWindowSize(window_size);
+        ImGui::SetNextWindowPos(window_pos);
 
-    ImGui::End();
-}
+        ImGui::Begin(label.data(), nullptr, window_flags);
 
-void render(WindowClass &window_obj)
-{
-    window_obj.Draw("Text editor");
-}
+
+        ImGui::End();
+    }
+
+    void WindowClass::DrawMenu() {}
+
+    void WindowClass::DrawContent() {}
+
+    void WindowClass::DrawInfo() {}
+
+    void WindowClass::DrawLoadPopup() {}
+
+    void WindowClass::DrawSavePopup() {}
+
+    void WindowClass::SaveToFile(std::string_view fileName) {}
+
+    void WindowClass::LoadToFile(std::string_view fileName) {}
+
+    std::string WindowClass::GetFileExtension(std::string_view fileName) {}
+
+    void render(WindowClass &window_obj) {
+        window_obj.Draw("Text editor");
+    }
+} // namespace
--- 4-imgui/app/3-text-editor/render.hpp
+++ 4-imgui/app/3-text-editor/render.hpp
@@ -2,11 +2,50 @@
 
 #include <cstdint>
 #include <string_view>
+#include <string>
+#include <cstring>
 
-class WindowClass
-{
-public:
-    void Draw(std::string_view label);
-};
+namespace ImGuiTextEditor {
+    class WindowClass {
+    public:
+        static constexpr auto bufferSize = std::size_t{1024};
+        static constexpr auto popUpFlag = ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
+                                          ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoScrollbar;
+        static constexpr auto popUpSize = ImVec2(300.0F, 100.0F);
+        static constexpr auto popUpButtonSize = ImVec2(120.0F, 0.0F);
+        static constexpr auto popUpPos = ImVec2(
+                1280.0F / 2.0F - popUpSize.x / 2.0F,
+                720.0F / 2.0F - popUpSize.y / 2.0F
+        );
 
-void render(WindowClass &window_obj);
+    public:
+        WindowClass() : currentFileName({}) {
+            std::memset(textBuffer, 0, bufferSize);
+        }
+
+        void Draw(std::string_view label);
+
+    private:
+        void DrawMenu();
+
+        void DrawSavePopup();
+
+        void DrawLoadPopup();
+
+        void DrawContent();
+
+        void DrawInfo();
+
+        void SaveToFile(std::string_view fileName);
+
+        void LoadToFile(std::string_view fileName);
+
+        std::string GetFileExtension(std::string_view fileName);
+
+    private:
+        char textBuffer[bufferSize];
+        std::string currentFileName;
+    };
+
+    void render(WindowClass &window_obj);
+}
