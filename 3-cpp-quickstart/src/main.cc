2997bc0c8592cb3984cb3d7ef9bb3409b45c4424
DigitalCrafting <k.kasprzyk2@gmail.com>
2025-02-02 12:58:41 +0100

    [Quickstart] Pointers

--- 3-cpp-quickstart/src/main.cc
+++ 3-cpp-quickstart/src/main.cc
@@ -1,8 +1,17 @@
 #include<iostream>
 
-#include "../include/my_lib.h"
+//#include "../include/my_lib.h"
 
 int main() {
-    print_hello_world();
+//    print_hello_world();
+    int intVar = 5;
+
+    int* intPointer = &intVar;
+
+    int** pointerPointer = &intPointer;
+
+    std::cout << "pointerPointer address -> pointerPointer value -> pointerPointer deref -> int" << "\n";
+    std::cout << &pointerPointer << " -> " << pointerPointer << " -> " << *pointerPointer << " -> "  << *(*pointerPointer) << "\n";
+
     return 0;
 }
