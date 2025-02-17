4c120c7bcdb791d3c2390504fa09b4f6b7898716
DigitalCrafting <k.kasprzyk2@gmail.com>
2025-02-17 14:50:59 +0100

    [ImGui] WallClock - part 2

--- 4-imgui/app/8-wall-clock/render.cpp
+++ 4-imgui/app/8-wall-clock/render.cpp
@@ -1,4 +1,6 @@
 #include <imgui.h>
+#include <complex>
+#include <chrono>
 
 #include "render.hpp"
 
@@ -15,15 +17,60 @@ namespace ImGuiWallClock {
 
         ImGui::Begin(label.data(), nullptr, window_flags);
 
+        const auto cursor_pos = ImGui::GetCursorScreenPos();
+        center = ImVec2(cursor_pos.x + circleRadius, cursor_pos.y + circleRadius);
+
+        DrawCircle(circleRadius);
+        GetTime();
+
+        const auto [hour_theta, minute_theta, second_theta] = GetTheta();
+
+        DrawClockHand(
+            circleRadius * hrsClockHandLength,
+            hour_theta,
+            ImColor(1.0F, 0.0F, 0.0F, 1.0F)
+        );
+        DrawClockHand(
+            circleRadius * minsClockHandLength,
+            minute_theta,
+            ImColor(0.0F, 1.0F, 0.0F, 1.0F)
+        );
+        DrawClockHand(
+            circleRadius * secsClockHandLength,
+            second_theta,
+            ImColor(0.0F, 0.0F, 1.0F, 1.0F)
+        );
+
+        DrawAllHoursStrokes();
+        DrawAllMinutesStrokes();
+
+        DrawCircle(innerRadius);
+        DrawDigitalClock();
+
         ImGui::End();
     }
 
     void WindowClass::DrawCircle(const float radius) {
-
+        ImGui::GetWindowDrawList()->AddCircle(
+                center,
+                radius,
+                ImGui::GetColorU32(ImGuiCol_Text),
+                100,
+                2.0F
+                );
     }
 
     void WindowClass::DrawClockHand(const float radius, const float theta, const ImColor color) {
+        const auto cos = std::cos(theta);
+        const auto sin = std::sin(theta);
+        const auto end_point = ImVec2(center.x - radius * cos, center.y - radius * sin);
 
+        ImGui::GetWindowDrawList()->AddLine(
+            center,
+            end_point,
+            color,
+            3.0F
+        );
     }
 
     void WindowClass::DrawAllHoursStrokes() {
@@ -35,15 +82,29 @@ namespace ImGuiWallClock {
     }
 
     void WindowClass::DrawDigitalClock() {
-
+        ImGui::Text("%d:%d:%d", hrs, mins, secs);
     }
 
     void WindowClass::GetTime() {
+        const auto timestamp_now = std::chrono::system_clock::now();
+        const auto time_now = std::chrono::system_clock::to_time_t(timestamp_now);
+        const auto time_struct = std::localtime(&time_now);
 
+        secs = time_struct->tm_sec;
+        mins = time_struct->tm_min;
+        hrs = time_struct->tm_hour;
     }
 
     std::tuple<float, float, float> WindowClass::GetTheta() {
-        return {};
+        const auto seconds_frac = static_cast<float>(secs);
+        const auto minutes_frac = static_cast<float>(mins) + seconds_frac / 60.0F;
+        const auto hours_frac = static_cast<float>(hrs) + minutes_frac / 60.0F;
+
+        const auto hours_theta = (hours_frac * ((2.0F * PI) / 12.0F)) + offset;
+        const auto minutes_theta = (minutes_frac * ((2.0F * PI) / 60.0F)) + offset;
+        const auto seconds_theta = (seconds_frac * ((2.0F * PI) / 60.0F)) + offset;
+
+        return std::make_tuple(hours_theta, minutes_theta, seconds_theta);
     }
 
     void render(WindowClass &window_obj) {
