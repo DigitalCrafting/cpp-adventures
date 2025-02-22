848fb8095743066591024ed1c3e2b8032da0e065
DigitalCrafting <k.kasprzyk2@gmail.com>
2025-02-22 20:37:24 +0100

    [ImGui] Final - initial commit

---
+++ 4-imgui/app/A-final/clock.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <cstdint>
+#include <numbers>
+#include <tuple>
+#include <string_view>
+#include "imgui.h"
+
+namespace ImGuiDesktop {
+    class Clock {
+    private:
+        static constexpr auto PI = std::numbers::pi_v<float>;
+        static constexpr auto circleRadius = 250.0F;
+        static constexpr auto offset = PI / 2.0F;
+        static constexpr auto innerRadius = 5.0F;
+        static constexpr auto hrsClockHandLength = 0.95F;
+        static constexpr auto minsClockHandLength = 0.85F;
+        static constexpr auto secsClockHandLength = 0.75F;
+        static constexpr auto hrsStrokesLength = 0.90F;
+        static constexpr auto minsStrokesLength = 0.95F;
+
+    public:
+        Clock(): secs(0), mins(0), hrs(0), center({}) {};
+
+        void Draw(std::string_view label);
+        void GetTime();
+
+    private:
+        void DrawCircle(const float radius);
+        void DrawClockHand(
+                const float radius,
+                const float theta,
+                const ImColor color
+        );
+        void DrawAllHoursStrokes();
+        void DrawAllMinutesStrokes();
+        void DrawDigitalClock();
+
+        std::tuple<float, float, float> GetTheta();
+
+    public:
+        std::int32_t secs;
+        std::int32_t mins;
+        std::int32_t hrs;
+
+    private:
+        ImVec2 center;
+    };
+
+    void renderClock(Clock &window_obj);
+
+} // ImGuiDesktop
---
+++ 4-imgui/app/A-final/render.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <string_view>
+#include <imgui.h>
+#include <string>
+#include <vector>
+
+#include "clock.h"
+#include "fmt/format.h"
+
+namespace ImGuiDesktop {
+    class WindowClass {
+    public:
+        constexpr static auto numIcons = std::uint32_t{10};
+
+        struct Icon {
+            Icon(std::string_view label_): label(label_), position(ImVec2{}), popupOpen(false), clickCount(0) {};
+
+            void Draw();
+            std::string label;
+            ImVec2 position;
+            bool popupOpen;
+            std::uint32_t clickCount;
+        };
+
+    public:
+        WindowClass() : icons(), clock({}) {
+            icons.reserve(numIcons);
+            for (std::uint32_t  i = 0; i < numIcons; ++i) {
+                icons.push_back(Icon{fmt::format("Icon {}", i)});
+            }
+        };
+
+        void Draw(std::string_view label);
+
+    private:
+        void DrawDesktop();
+        void DrawTaskbar();
+
+        void ShowIconList(bool *open = nullptr);
+
+    private:
+        std::vector<Icon> icons;
+        Clock clock;
+    };
+
+    void render(WindowClass &window_obj);
+}
