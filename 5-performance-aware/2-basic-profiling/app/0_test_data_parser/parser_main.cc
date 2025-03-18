6bed9e24a99a5a9c1c5f4f091bcc4e67ca00a6df
DigitalCrafting <k.kasprzyk2@gmail.com>
2025-03-18 10:58:52 +0100

    [PerformanceAware] Profiling - json lookup parser impl WIP 5

--- 5-performance-aware/2-basic-profiling/app/0_test_data_parser/parser_main.cc
+++ 5-performance-aware/2-basic-profiling/app/0_test_data_parser/parser_main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sys/stat.h>
 
 #include "haversine.h"
 #include "profiler.h"
@@ -6,6 +7,30 @@
 #include "common_random.h"
 #include "common_json.h"
 #include "base_json_lookup_parser.h"
+
+static Buffer ReadEntireFile(char *filename) {
+    Buffer result = {};
+
+    FILE *file = fopen(filename, "rb");
+    if (file) {
+        struct stat statStruct;
+        stat(filename, &statStruct);
+
+        result = AllocateBuffer(statStruct.st_size);
+        if (result.data) {
+            if(fread(result.data, result.count, 1, file) != 1) {
+                fprintf(stderr, "ERROR: Unable to read \"%s\".\n", filename);
+                FreeBuffer(&result);
+            }
+        }
+
+        fclose(file);
+    } else {
+        fprintf(stderr, "ERROR: Unable to read \"%s\".\n", filename);
+    }
+
+    return result;
+}
 
 /*
  * Parses a file with longitudes and latitudes pairs, for testing haversine performance, in JSON format.
@@ -34,6 +59,10 @@ void parse_test_data() {
 
 int main() {
     parse_test_data();
+
+    Buffer file = ReadEntireFile("/home/kamil/Projects/github/cpp-adventures/5-performance-aware/2-basic-profiling/cmake-build-debug/app/0_test_data_generator/data_10_flex.json");
+
+    std::cout << "Read file size: " << file.count << '\n';
 
     return 0;
 }
