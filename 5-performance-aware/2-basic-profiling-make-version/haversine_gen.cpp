cafd9ff09e2463e36668db660b66cf21152fd264
DigitalCrafting <k.kasprzyk2@gmail.com>
2025-03-20 17:56:22 +0100

    [PerformanceAware][Profiling][Make] Reading command line args

--- 5-performance-aware/2-basic-profiling-make-version/haversine_gen.cpp
+++ 5-performance-aware/2-basic-profiling-make-version/haversine_gen.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <cstdlib>
+#include <cstdint>
+
+void generate(int nmbrOfPairs) {
+    std::cout << "Hello" << nmbrOfPairs << '\n';
+}
 
 int main(int argc, char **args) {
-    std::cout << "Hello\n";
+    int nmbrOfPairs = 100;
+
+    if (argc == 2) {
+        nmbrOfPairs = atoi(args[1]);
+        if (nmbrOfPairs >= UINT64_MAX) {
+            fprintf(stderr, "To avoid accidentally generating massive files, number of pairs must be less than %lu.\n", UINT64_MAX);
+            return 1;
+        }
+    }
+
+
+    generate(nmbrOfPairs);
     return 0;
 }
