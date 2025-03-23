da0367b1aa68caf674e2f4635a21225edadf87c7
DigitalCrafting <k.kasprzyk2@gmail.com>
2025-03-23 12:24:18 +0100

    [PerformanceAware][Profiling][Make] Finished generator

--- 5-performance-aware/2-basic-profiling-make-version/haversine_gen.cpp
+++ 5-performance-aware/2-basic-profiling-make-version/haversine_gen.cpp
@@ -2,10 +2,37 @@
 #include <cstdlib>
 #include <cstdint>
 
-#include "common_types.h"
+#include "common/common_types.h"
+#include "common/common_random.h"
+#include "common/common_file.h"
 
-void generate(int nmbrOfPairs) {
-    std::cout << "Hello" << nmbrOfPairs << '\n';
+void generate(int pairs) {
+    f64 maxAllowedX = 180;
+    f64 maxAllowedY = 90;
+
+    f64 xCenter = 0;
+    f64 yCenter = 0;
+    f64 xRadius = maxAllowedX;
+    f64 yRadius = maxAllowedY;
+
+    RandomSeries seed = Seed(31);
+
+    FILE* testDataFile = Open(pairs, "flex", "json");
+    if (testDataFile) {
+        fprintf(testDataFile, "{\"pairs\n: [\n");
+        for (int i = 0; i < pairs; ++i) {
+            f64 x0 = RandomDegree(&seed, xCenter, xRadius, maxAllowedX);
+            f64 y0 = RandomDegree(&seed, yCenter, yRadius, maxAllowedY);
+            f64 x1 = RandomDegree(&seed, xCenter, xRadius, maxAllowedX);
+            f64 y1 = RandomDegree(&seed, yCenter, yRadius, maxAllowedY);
+
+            const char* jsonSeparator = (i == (pairs - 1)) ? "\n" : ",\n";
+
+            fprintf(testDataFile, "    {\"x0\": %.16f,\"y0\": %.16f,\"x1\": %.16f,\"y1\": %.16f}%s", x0, y0, x1, y1, jsonSeparator);
+        }
+    }
+
+    fprintf(testDataFile, "]}\n");
 }
 
 int main(int argc, char **args) {
