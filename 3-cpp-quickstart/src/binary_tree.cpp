da86e5d60ac8f3690d2e41c7e1dabb9c4a46408c
DigitalCrafting <k.kasprzyk2@gmail.com>
2025-04-07 18:25:43 +0200

    [Quickstart] Binary tree getValuesAtDepth

--- 3-cpp-quickstart/src/binary_tree.cpp
+++ 3-cpp-quickstart/src/binary_tree.cpp
@@ -1,8 +1,10 @@
 #include <climits>
 #include <iostream>
 #include <string>
-#include <array>
-#include <cstdint>
+#include <vector>
+#include <cmath>
+
+#define MyNAN std::numeric_limits<int>::quiet_NaN()
 
 struct Node {
     int value;
@@ -22,6 +24,21 @@ struct Node {
             dRight = right->depth();
 
         return 1 + (dLeft > dRight ? dLeft : dRight);
+    }
+
+    int getLeftChildValue() {
+        if (left) {
+            return left->value;
+        }
+
+        return MyNAN;
+    }
+
+    int getRightChildValue() {
+        if (right) {
+            return right->value;
+        }
+        return MyNAN;
     }
 
 };
@@ -70,14 +87,64 @@ std::string leftPad(std::string str, unsigned int pads) {
     return spaces + str;
 }
 
-std::array<int, 2> getValuesAtDepth(Node *root, int depth) {
-    std::array<int, 2> result = {INT_MAX, INT_MAX};
+/*
+ * Struct representing a touple of values - left and right.
+ *
+ * Each depth of the Binary Tree will consist of multiple such pairs.
+ *
+ * */
+struct ValueAtDepth {
+    int left;
+    int right;
 
+    ValueAtDepth(int _left, int _right): left(_left), right(_right){}
+};
+
+std::vector<ValueAtDepth> getValuesAtDepth(Node *root, int depth) {
+    /*
+     * If current node is nullptr, then we need to fill the childrend to preserve spacing
+     */
+    if (root == nullptr) {
+        std::vector<ValueAtDepth> result;
+        for (int i = 0; i <= (2^depth); i++) {
+            result.push_back(ValueAtDepth(MyNAN, MyNAN));
+        }
+        return result;
+    }
+
+
+    if (depth == 0) {
+        return std::vector<ValueAtDepth>{ValueAtDepth(root->value, MyNAN)};
+    }
+
+    if (depth == 1) {
+        int leftVal = root->getLeftChildValue();
+        int rightVal = root->getRightChildValue();
+
+        return std::vector<ValueAtDepth>{ValueAtDepth(leftVal, rightVal)};
+    }
+
+    auto leftValues = getValuesAtDepth(root->left, depth - 1);
+    auto rightValues = getValuesAtDepth(root->right, depth - 1);
+    std::vector<ValueAtDepth> result = {};
+    result.insert(result.end(), leftValues.begin(), leftValues.end());
+    result.insert(result.end(), rightValues.begin(), rightValues.end());
     return result; 
 }
 
 void prettyPrint(Node *root) {
     int depth = root->depth() - 1; // -1 to start from 0
+
+    for (int i = 0; i <= depth; i++) {
+        auto values = getValuesAtDepth(root, i);
+        for (auto val : values) {
+            std::cout << (val.left != MyNAN ? std::to_string(val.left) : " - "); 
+            std::cout << " "; 
+            std::cout << (val.right != MyNAN ? std::to_string(val.right) : " - "); 
+            std::cout << " ";
+        }
+        std::cout << "\n";
+    }
 
     std::cout << "Depth: " << depth <<"\n";
 }
