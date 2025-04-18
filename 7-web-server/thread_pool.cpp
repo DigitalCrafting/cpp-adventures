4426012d976a4853b0884de3092e8bdd4747ed97
DigitalCrafting <k.kasprzyk2@gmail.com>
2025-04-18 20:28:39 +0200

    [WebServer] ThreadPool

--- 7-web-server/thread_pool.cpp
+++ 7-web-server/thread_pool.cpp
@@ -6,6 +6,7 @@
 #include <condition_variable>
 #include <atomic>
 #include <functional>
+#include <chrono>
 
 
 class ThreadPool {
@@ -19,11 +20,39 @@ private:
     std::atomic<bool> stop;
 
 public:
-    ThreadPool(size_t _threads): stop(false) {
+    ThreadPool(size_t _threads) : stop(false) {
         for (int i = 0; i < _threads; ++i) {
-            // TODO
-            std::cout << "Initializing thread " << i << " of ThreadPool\n";
+            workers.emplace_back([this]() {
+                while (true) {
+                    std::function<void()> task;
+
+                    {
+                        std::unique_lock<std::mutex> lock(this->queue_mutex);
+                        this->condition.wait(lock, [this]() {
+                            return this->stop || !this->tasks.empty();
+                        });
+
+                        if (this->stop && this->tasks.empty()) {
+                            return;
+                        }
+
+                        task = std::move(this->tasks.front());
+                        this->tasks.pop();
+                    }
+
+                    task();
+                }
+            });
         }
+    }
+
+    void enqueue(std::function<void()> task) {
+        {
+            std::unique_lock<std::mutex> lock(queue_mutex);
+            if (stop) throw std::runtime_error("Called enqueue on stopped pool.");
+            tasks.emplace(std::move(task));
+        }
+        condition.notify_one();
     }
 
     ~ThreadPool() {
@@ -34,7 +63,7 @@ public:
         }
 
         condition.notify_all();
-        for (std::thread &worker : workers) {
+        for (std::thread &worker: workers) {
             worker.join();
         }
     }
@@ -43,5 +72,21 @@ public:
 int main() {
     std::cout << "Hello from thread pool\n";
     ThreadPool threadPool{2};
+    threadPool.enqueue([] {
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        std::cout << "First task\n";
+    });
+    threadPool.enqueue([] {
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        std::cout << "Second task\n";
+    });
+    threadPool.enqueue([] {
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        std::cout << "Third task\n";
+    });
+    threadPool.enqueue([] {
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        std::cout << "Fourth task\n";
+    });
     return 0;
 }
