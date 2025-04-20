f7188027390df7bae6e1792afac439c7a8f4bb42
DigitalCrafting <k.kasprzyk2@gmail.com>
2025-04-20 19:22:52 +0200

    [WebServer] ConnectionManager WIP

--- 7-web-server/connection_manager.cpp
+++ 7-web-server/connection_manager.cpp
@@ -1,6 +1,11 @@
 #include <unordered_map>
 #include <string>
+#include <vector>
 #include <chrono>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <iostream>
 
 struct Connection {
     int socket_fd;
@@ -12,7 +17,48 @@ struct Connection {
 class ConnectionManager {
 public:
     ConnectionManager(int _server_fd): server_fd(_server_fd) {}
-    void run();
+    void run() {
+        while (true) {
+            fd_set read_fds, write_fds;
+            FD_ZERO(&read_fds);
+            FD_ZERO(&write_fds);
+
+            FD_SET(server_fd, &read_fds);
+            int max_fd = server_fd;
+
+            for (auto &[fd, conn] : connections) {
+                FD_SET(fd, &read_fds);
+                if (!conn.writeBuffer.empty()) {
+                    FD_SET(fd, &write_fds);
+                }
+                if (fd > max_fd) max_fd = fd;
+            }
+
+            struct timeval tv = {1, 0};
+            int activity = select(max_fd + 1, &read_fds, &write_fds, nullptr, &tv);
+
+            if (activity < 0) {
+                perror("select");
+                break;
+            }
+
+            if (FD_ISSET(server_fd, &read_fds)) {
+                handleNewConnection();
+            }
+
+            std::vector<int> toRemove;
+            for (auto &[fd, conn] : connections) {
+                if (FD_ISSET(fd, &read_fds)) {
+                    handleClientRead(fd);
+                }
+                if (FD_ISSET(fd, &write_fds)) {
+                    handleClientWrite(fd);
+                }
+            }
+
+            cleanUpDisconnected();
+        }
+    }
 
 private:
     void handleNewConnection() {}
