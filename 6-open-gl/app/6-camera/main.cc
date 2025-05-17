aa70baf430bea0ff3f71fcbc865e269f3e0d8525
DigitalCrafting <k.kasprzyk2@gmail.com>
2025-05-17 20:27:22 +0200

    [OpenGl] Camera - WASD movement

--- 6-open-gl/app/6-camera/main.cc
+++ 6-open-gl/app/6-camera/main.cc
@@ -19,6 +19,9 @@ void glfw_error_callback(int error, const char *description) {
 }
 
 static float mixValue = 0.2f;
+static glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  3.0f);
+static glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+static glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
 
 void processInput(GLFWwindow *window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
@@ -35,6 +38,20 @@ void processInput(GLFWwindow *window) {
         if (mixValue <= 0.0f) {
             mixValue = 0.0f;
         }
+    }
+
+    const float cameraSpeed = 0.01f;
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+        cameraPos += cameraSpeed * cameraFront;
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+        cameraPos -= cameraSpeed * cameraFront;
+    }
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    }
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
     }
 }
 
@@ -179,19 +196,19 @@ int main() {
     shaderProgram.setInt("texture1", 0);
     shaderProgram.setInt("texture2", 1);
 
-    glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
-    glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
-    glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+//    glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+//    glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+//    glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
 
-    glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
-    glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
+//    glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+//    glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
 
-    glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
+//    glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
 
-    glm::mat4 view;
-    view = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f),
-                       glm::vec3(0.0f, 0.0f, 0.0f),
-                       glm::vec3(0.0f, 1.0f, 0.0f));
+//    glm::mat4 view;
+//    view = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f),
+//                       glm::vec3(0.0f, 0.0f, 0.0f),
+//                       glm::vec3(0.0f, 1.0f, 0.0f));
 
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
@@ -210,11 +227,11 @@ int main() {
 
 //        model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
 //        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-        const float radius = 10.0f;
-        float camX = sin(glfwGetTime()) * radius;
-        float camZ = cos(glfwGetTime()) * radius;
+//        const float radius = 10.0f;
+//        float camX = sin(glfwGetTime()) * radius;
+//        float camZ = cos(glfwGetTime()) * radius;
         glm::mat4 view;
-        view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+        view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 
         projection = glm::perspective(glm::radians(45.0f), (float) width / (float) height, 0.1f, 100.0f);
 
